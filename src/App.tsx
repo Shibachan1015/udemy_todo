8b689c8cccc63609ce8f117a5eb398850f457326
Taka Shiba <takashiba@TakanoMacBook-Pro.local>
2021-04-30 23:02:23 +0900

    "second branch first commit"

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,28 @@ import { FormControl, TextField } from "@material-ui/core";
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { db } from "./firebase";
-
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
   const [input, setInput] = useState("");
 
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot)=>{
-      setTasks(
+      setTasks( //snapshotにより常にCloud Firestoreを監視している
         snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
       );
     });
-    return () => unSub();
+    return () => unSub(); //クリーンナップ関数　ページのリフレッシュなどによりアンマウントされたあとDomを解放してあげる関数
   }, []);
+
+  const newTask = (e: React.MouseEventHandler<HTMLButtonElement>)=>{
+    db.collection("tasks").add({ title: input });
+    setInput("");
+  };
 
   return (
     <div className="App">
+      <h1>Todo App by React/Fierbase</h1>
       <FormControl>
         <TextField
           InputLabelProps={{
@@ -26,10 +32,13 @@ const App: React.FC = () => {
           label="New task ?"
           value={input}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setInput(e.target.value)
+            setInput(e.target.value) //ユーザーが入力した値valueを８行目のsetInputに代入している
           }
         />
       </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon />
+      </button>
 
       {tasks.map((task) => (
         <h3 key={task.id}>{task.title}</h3>
